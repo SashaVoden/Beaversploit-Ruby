8af84994eb02ec590ec8fb2275778e5e9df3a7dd
SashaVoden <alexvodennikov22013@gmail.com>
2025-06-05 14:42:16 +0300

    plugins support update

--- core.rb
+++ core.rb
@@ -1,13 +1,15 @@
 module BeaverSploit
   class Core
-    attr_reader :modules
+    attr_reader :plugins, :modules
 
     def initialize
+      @plugins = {}
       @modules = {}
-      load_modules
+      load_plugins   # Загружаем плагины первыми
+      load_modules   # Затем модули
     end
 
-    def instantiate_module_from_file(file)
+    def instantiate_file(file)
       before = Module.constants.dup
       require file
       after = Module.constants
@@ -16,10 +18,7 @@ module BeaverSploit
       new_consts.each do |const|
         begin
           candidate = Module.const_get(const)
-          if candidate.is_a?(Class) &&
-             candidate.instance_methods.include?(:run) &&
-             candidate.instance_methods.include?(:set_option) &&
-             candidate.instance_methods.include?(:show_options)
+          if candidate.is_a?(Class) && candidate.instance_methods.include?(:run)
             new_instance = candidate.new
             break
           end
@@ -30,18 +29,38 @@ module BeaverSploit
       new_instance
     end
 
+    def load_plugins
+      base = File.join(__dir__, 'plugins')
+      Dir.glob(File.join(base, '**', '*.rb')).each do |file|
+        relative_key = file.sub("#{__dir__}/", "").gsub("\\", "/").downcase
+        plugin_instance = instantiate_file(file)
+        if plugin_instance
+          @plugins[relative_key] = plugin_instance
+          puts "[+] Loaded plugin: #{relative_key}"
+        else
+          puts "[-] Failed to load plugin: #{relative_key}"
+        end
+      end
+    end
+
     def load_modules
       base = File.join(__dir__, 'modules')
       Dir.glob(File.join(base, '**', '*.rb')).each do |file|
         relative_key = file.sub("#{__dir__}/", "").gsub("\\", "/").downcase
-        module_instance = instantiate_module_from_file(file)
+        module_instance = instantiate_file(file)
         if module_instance
           @modules[relative_key] = module_instance
           puts "[*] Loaded module: #{relative_key}"
         else
-          puts "[!] Failed to instantiate module: #{relative_key}"
+          puts "[!] Failed to load module: #{relative_key}"
         end
       end
+    end
+
+    def list_plugins
+      puts "\n*** Available Plugins ***"
+      @plugins.keys.each { |key| puts " - #{key}" }
+      puts "*************************"
     end
 
     def list_modules
@@ -50,9 +69,24 @@ module BeaverSploit
       puts "*************************"
     end
 
-    def get_module(key)
-      key = key.downcase
-      @modules[key]
+    def use_plugin(plugin_key)
+      plugin = @plugins[plugin_key.downcase]
+      if plugin
+        puts "[+] Using plugin: #{plugin_key}"
+        plugin.run
+      else
+        puts "[-] Plugin '#{plugin_key}' not found."
+      end
+    end
+
+    def use_module(module_key)
+      mod = @modules[module_key.downcase]
+      if mod
+        puts "[+] Using module: #{module_key}"
+        mod.run
+      else
+        puts "[-] Module '#{module_key}' not found."
+      end
     end
   end
 end
